8350cacdc24e9ff579d9513fbf8e0be11d9d70b6
William Qi <hello@williamqi.com>
2014-10-18 00:52:36 -0700

    Big change to code, now able to stream from soundcloud with placeholder cork

--- launch.js
+++ launch.js
@@ -1,8 +1,24 @@
+SC.initialize({
+      client_id: "be26e36572618f7b450125e3b4f68854"
+});
+
 $('.playButton').on('click',function() {
-    soundManager.createSound({
-        url: this.dataset.sound
-    }).play();
+	if (this.dataset.type == "local") {
+    	soundManager.createSound({
+        	url: this.dataset.sound
+    	}).play();
+	}
+	else if (this.dataset.type == "soundcloud") {
+		var track_url = this.dataset.sound;
+		SC.get('/resolve', {url: track_url}, function(track) {
+			SC.stream('/tracks/' + track.id, function(sound) {
+				sound.play();
+			});
+		})
+		
+	}
 })
+
 
 function playSound(e) {
 
