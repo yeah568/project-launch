79f002d984125170adbec4c6908b5bcfe4b48245
James Liu <jamesjhliu@gmail.com>
2014-10-18 04:22:37 -0700

    things? i don't even know anymore

--- launch.js
+++ launch.js
@@ -3,24 +3,87 @@ SC.initialize({
 });
 
 $('.playButton').on('click',function() {
-	if (this.dataset.type == "local") {
+/*	if (this.dataset.type == "local") {
     	soundManager.createSound({
         	url: this.dataset.sound
     	}).play();
 	}
 	else if (this.dataset.type == "soundcloud") {
-		var track_url = this.dataset.sound;
-		SC.get('/resolve', {url: track_url}, function(track) {
-			SC.stream('/tracks/' + track.id, function(sound) {
-				sound.play();
-			});
-		})
-		
-	}
+
+
+	}*/
+    soundManager.getSoundById(this.dataset.soundid).play();
 })
+
+var soundids = [];
+
+
+var buttons = [];
+
+$('li').each(function(index, e) {
+    $('#buttonSelect').append($('<option>', {
+        value: index,
+        text: index
+    }));
+    $('#buttonSelectFile').append($('<option>', {
+        value: index,
+        text: index
+    }));
+    buttons.push(e);
+})
+
 
 function setSoundSource(reference, element) {
 	element.dataset.sound = reference;
+}
+
+function addSC() {
+    var track_url = $("#scurl").val();
+    SC.get('/resolve', {url: track_url}, function(track) {
+        SC.stream('/tracks/' + track.id, {autoLoad: true}, function(sound) {
+            soundids.push(sound.id);
+            buttons[$('#buttonSelect').val()].dataset.soundid = sound.id;
+        });
+    })
+};
+
+//$('#fileInput').addEventListener('change', addFile, false);
+
+function addFile(files) {
+    var file = files[0];
+
+    var reader = new FileReader();
+
+    reader.onloadend = function () {
+        buttons[$('#buttonSelectFile').val()].dataset.soundid = soundManager.createSound({
+            url: reader.result
+        }).id;
+    }
+
+    reader.readAsDataURL(file);
+
+}
+
+function playSound(e) {
+
+
+}
+
+function sliceSound(e) {
+    switch(e.dataset.type) {
+        case "local":
+
+            break;
+        case "soundcloud":
+
+            break;
+
+    }
+
+    // get track length
+
+    // set slider to 0/length
+
 }
 
 
