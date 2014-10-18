88a2fef180572862bc2955e5234e675bce42538a
William Qi <hello@williamqi.com>
2014-10-18 02:06:24 -0700

    added logic for changing source of sound

--- launch.js
+++ launch.js
@@ -19,15 +19,13 @@ $('.playButton').on('click',function() {
 	}
 })
 
-
-function playSound(e) {
-
+function setSoundSource(reference, element) {
+	element.dataset.sound = reference;
 }
 
 
 
-// parseSound should look up the button in the table and find the
-// assigned sound and return a reference to it
+
 
 // play should eventually be able to implement defined behaviour of
 // the button,
